6d27cfd81caf422d743c1f7b7e1f52d2c280cf0a
yatsun <yatsunlam25@gmail.com>
2021-12-06 20:26:42 -0500

    commit tree 490428b31b2dba109f6b91bd26cc67c603a975a0

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  state = {
+    text: ''
+  }
+  handleOnSubmit = event => {
+    event.preventDefault()
+    this.props.addReview({text: this.state.text, restaurantId: this.props.restaurantId})
+    this.setState({
+      text: ''
+    })
+  }
+  handleOnChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleOnSubmit} >
+          <label>Add Review</label>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handleOnChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
